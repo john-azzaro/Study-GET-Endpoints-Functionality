3cf9046c01ee7b2938bc63e8f526833713d8c374
John Azzaro <jazzaro@temple.edu>
2019-08-01 17:24:37 -0700

    adjusted server js, pn additions

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 
-const express = require('express');
-// we'll use morgan to log the HTTP layer
-const morgan = require('morgan');
-// we'll use body-parser's json() method to 
-// parse JSON data sent in requests to this app
-const bodyParser = require('body-parser');
+const express = require('express');                                                          
+const morgan = require('morgan');                                                         // we'll use morgan to log the HTTP layer 
+const bodyParser = require('body-parser');                                                // use body-parser's json() method to parse JSON data sent in requests to this app
 
-// we import the ShoppingList model, which we'll
-// interact with in our GET endpoint
-const {ShoppingList} = require('./models.js');
+const {ShoppingList} = require('./models.js');                                            // we import the ShoppingList model, which we'll interact with in our GET endpoint
 
-const jsonParser = bodyParser.json();
-const app = express();
+const jsonParser = bodyParser.json();                                                     
+const app = express();                                                                     // use app to run express.
 
-// log the http layer
-app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
-// normally you wouldn't do this. Usually your
-// server will simply expose the state of the
-// underlying database.
-ShoppingList.create('beans', 2);
+app.use(morgan('common'));                                                                 // log the http layer when we use node (and nodemon) while running.
+
+
+ShoppingList.create('beans', 2);                                                            // demonstration list items.Note that normally you wouldn't do this. Usually your server will simply expose the state of the underlying database.
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
-app.get('/shopping-list', (req, res) => {
+// 
+// 
+app.get('/shopping-list', (req, res) => {                                                  // when the root of this route is called with GET, returnall current ShoppingList items by calling `ShoppingList.get()`
   res.json(ShoppingList.get());
 });
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {                                                // Listen on port 8080.
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
